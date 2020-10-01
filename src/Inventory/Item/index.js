1235c1addc9b437b1bb7c81eb22a46289579b7f9
Sumsun <sumsun93@gmail.com>
2020-10-01 21:55:50 +0200

    Add context menu

--- src/Inventory/Item/index.js
+++ src/Inventory/Item/index.js
@@ -2,6 +2,7 @@
  * Global import
  */
 import React from "react";
+import { ContextMenuTrigger, ContextMenu, MenuItem } from "react-contextmenu";
 
 /**
  * Local import
@@ -15,16 +16,28 @@ import Drag from './Drag';
  * Component
  */
 class Item extends React.Component {
+  handleAction = (type) => () => {
+    console.log(`DO ACTION ${type}`)
+  }
+
   render() {
     const { data } = this.props;
     return (
       <Drag {...this.props}>
-        <div className="item">
-          {/* <img className="bg_stripes" src={bg_stripes} /> */}
-          <img className="Item_IMG" src={Item_IMG} alt="item" />
-          <div className="Item_Text">{data.name}</div>
-          <div className="Item_Quantity">{data.quantity}x</div>
-        </div>
+        <ContextMenuTrigger id={data.id.toString()}>
+          <div className="item">
+            {/* <img className="bg_stripes" src={bg_stripes} /> */}
+            <img className="Item_IMG" src={Item_IMG} alt="item" />
+            <div className="Item_Text">{data.name}</div>
+            <div className="Item_Quantity">{data.quantity}x</div>
+          </div>
+          <ContextMenu id={data.id.toString()} className="context_menu">
+            <MenuItem onClick={this.handleAction(1)} className="context_item">Action 1</MenuItem>
+            <MenuItem onClick={this.handleAction(2)} className="context_item">Action 2</MenuItem>
+            <MenuItem onClick={this.handleAction(3)} className="context_item">Action 3</MenuItem>
+            <MenuItem onClick={this.handleAction(4)} className="context_item">Action 4</MenuItem>
+          </ContextMenu>
+        </ContextMenuTrigger>
       </Drag>
     );
   }
